a4f9ad868ced8d50c891d829efbad780d294a83d
sathvik06767-svg <sathvik.067.67@gmail.com>
2026-09-18 14:24:42 +0530

    Add files via upload

---
+++ python1.py
@@ -0,0 +1,79 @@
+print("Hello World!.")
+print("Welcome to Python.")
+print("sathvik")
+print("I am learning Python.")
+print("Python Program.")
+print("Good Morning.")
+print("Welcome Students.")
+print("python is easy.")
+print("My First Program.")
+print("Hello from python!.")
+print("Hello from python!.")
+print(2+5)
+print("2+5")
+print("10-3")
+print(10-3)
+print("Hello World!")
+print("Python")
+print("Hello")
+print("Hello")
+print(10+20)
+name = "sathvik"
+age = 20
+marks = 85
+price = 99.5
+city=("Chennai")
+x=10
+print(x)
+message = "Welcome"
+a = 10 
+b = 20
+print(a+b)
+quantity = 5
+price = 10 
+print(quantity*price)
+print(type(25))
+print(type(25.5))
+print(type('25'))
+age=21
+height=5.8
+name='arun' 
+age=20
+price=99.5
+name='ravi'
+marks=90
+print(type(marks))
+temp=36.5
+print(type(temp))
+message='name'
+print(type(name))
+print('start')
+print('middle')
+print('end')
+x=10
+print(x)
+x=20
+print(x)
+number=5
+print(number)
+number=10
+print(number)
+print('A,B,C')
+name='ravi'
+age=20
+print(name)
+print(age)
+print(1)
+print(2)
+print(3)
+print(4)
+x=5
+y=10
+print(x)
+print(y)
+print('start')
+print(2+3)
+print('end')
+print('login')
+print('dashboard')
+print('logout')
